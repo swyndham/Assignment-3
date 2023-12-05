ed3a37fb63d57850e424dc0c6b944f943878f409
s.wyndham <s.wyndham@uleth.ca>
2023-12-04 18:44:54 -0700

    restard code

--- js/script.js
+++ js/script.js
@@ -2,92 +2,92 @@ var r = Math.floor(Math.random() * 256);
 
 $(document).on("keypress", function (e) {
     if (e.key == "r") {
-var counter = 0;
-$("#beam").on("click", function () {
-    $("#beam .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#ufobase").on("click", function () {
-    $("#ufobase .cls-3").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#glass").on("click", function () {
-    $("#glass .cls-3").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#stars").on("click", function () {
-    $("#stars .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#startrail").on("click", function () {
-    $("#startrail .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#startrailmiddle").on("click", function () {
-    $("#startrailmiddle .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#star").on("click", function () {
-    $("#star .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#moondivots").on("click", function () {
-    $("#moondivots .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#moons").on("click", function () {
-    $("#moons .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#planet").on("click", function () {
-    $("#planet .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#moons").on("click", function () {
-    $("#moons .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#stripes").on("click", function () {
-    $("#stripes .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-
-$("#background").on("click", function () {
-    $("#background .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
-})
-    } else if (e.key == "s") {
         var counter = 0;
         $("#beam").on("click", function () {
-            $("#beam .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 199)) + "," + (Math.floor(Math.random() * 123+133)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+            $("#beam .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
         })
 
         $("#ufobase").on("click", function () {
-            $("#ufobase .cls-3").css("fill", "rgb(" + (Math.floor(Math.random() * 55+45)) + "," + (Math.floor(Math.random() * 55+45)) + "," + (Math.floor(Math.random() * 55+45)) + ")"[counter])
+            $("#ufobase .cls-3").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
         })
 
         $("#glass").on("click", function () {
-            $("#glass .cls-3").css("fill", "rgb(" + (Math.floor(Math.random() * 29+90)) + "," + (Math.floor(Math.random() * 54+90)) + "," + (Math.floor(Math.random() * 54+90)) + ")"[counter])
+            $("#glass .cls-3").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
         })
 
         $("#stars").on("click", function () {
-            $("#stars .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 7+248)) + "," + 255 + "," + (Math.floor(Math.random() * 165+90)) + ")"[counter])
-        })
-
-        $("#star").on("click", function () {
-            $("#star .cls-2").css("fill", "rgb(" + 255 + "," + (Math.floor(Math.random() * 39+216)) + "," + (Math.floor(Math.random() * 187+68)) + ")"[counter])
-        })
-        
-        $("#moons").on("click", function () {
-            $("#moons .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 57+197)) + "," + (Math.floor(Math.random() * 57+197)) + "," + (Math.floor(Math.random() * 57+197)) + ")"[counter])
-        })
-
-        $("#moondivots").on("click", function () {
-            $("#moondivots .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 62+125)) + "," + (Math.floor(Math.random() * 62+125)) + "," + (Math.floor(Math.random() * 62+125)) + ")"[counter])
+            $("#stars .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
         })
 
         $("#startrail").on("click", function () {
             $("#startrail .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
         })
-        
+
+        $("#startrailmiddle").on("click", function () {
+            $("#startrailmiddle .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+        })
+
+        $("#star").on("click", function () {
+            $("#star .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+        })
+
+        $("#moondivots").on("click", function () {
+            $("#moondivots .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+        })
+
+        $("#moons").on("click", function () {
+            $("#moons .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+        })
+
+        $("#planet").on("click", function () {
+            $("#planet .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+        })
+
+        $("#moons").on("click", function () {
+            $("#moons .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+        })
+
+        $("#stripes").on("click", function () {
+            $("#stripes .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+        })
+
+        $("#background").on("click", function () {
+            $("#background .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+        })
+    } else if (e.key == "s") {
+        var counter = 0;
+        $("#beam").on("click", function () {
+            $("#beam .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 199)) + "," + (Math.floor(Math.random() * 123 + 133)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+        })
+
+        $("#ufobase").on("click", function () {
+            $("#ufobase .cls-3").css("fill", "rgb(" + (Math.floor(Math.random() * 55 + 45)) + "," + (Math.floor(Math.random() * 55 + 45)) + "," + (Math.floor(Math.random() * 55 + 45)) + ")"[counter])
+        })
+
+        $("#glass").on("click", function () {
+            $("#glass .cls-3").css("fill", "rgb(" + (Math.floor(Math.random() * 29 + 90)) + "," + (Math.floor(Math.random() * 54 + 90)) + "," + (Math.floor(Math.random() * 54 + 90)) + ")"[counter])
+        })
+
+        $("#stars").on("click", function () {
+            $("#stars .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 7 + 248)) + "," + 255 + "," + (Math.floor(Math.random() * 165 + 90)) + ")"[counter])
+        })
+
+        $("#star").on("click", function () {
+            $("#star .cls-2").css("fill", "rgb(" + 255 + "," + (Math.floor(Math.random() * 39 + 216)) + "," + (Math.floor(Math.random() * 187 + 68)) + ")"[counter])
+        })
+
+        $("#moons").on("click", function () {
+            $("#moons .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 57 + 197)) + "," + (Math.floor(Math.random() * 57 + 197)) + "," + (Math.floor(Math.random() * 57 + 197)) + ")"[counter])
+        })
+
+        $("#moondivots").on("click", function () {
+            $("#moondivots .cls-2").css("fill", "rgb(" + (Math.floor(Math.random() * 62 + 125)) + "," + (Math.floor(Math.random() * 62 + 125)) + "," + (Math.floor(Math.random() * 62 + 125)) + ")"[counter])
+        })
+
+        $("#startrail").on("click", function () {
+            $("#startrail .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
+        })
+
         $("#startrailmiddle").on("click", function () {
             $("#startrailmiddle .cls-1").css("fill", "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")"[counter])
         })
@@ -101,7 +101,12 @@ $("#background").on("click", function () {
         })
 
         $("#background").on("click", function () {
-            $("#background .cls-1").css("fill", "rgb(" + 0 + "," + (Math.floor(Math.random() * 37+5)) + "," + (Math.floor(Math.random() * 109+13)) + ")"[counter])
+            $("#background .cls-1").css("fill", "rgb(" + 0 + "," + (Math.floor(Math.random() * 37 + 5)) + "," + (Math.floor(Math.random() * 109 + 13)) + ")"[counter])
         })
+    } else if (e.key == "w") {
+        var counter = 0;
+        $(".cls-1").css("fill", "white");
+        $(".cls-2").css("fill", "white");
+        $(".cls-3").css("fill", "white");
     }
 })
